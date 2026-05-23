12fb535150ff12ca47410805b44142b3157dc6c8
mwolfe13 <mtwolfe2@my.loyno.edu>
2026-05-22 19:00:47 -0500

    fix dirname error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  experimental: {
+    serverComponentsExternalPackages: ['@supabase/ssr'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
