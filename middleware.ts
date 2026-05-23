7fe2ffa832f0e44363ab8236960e9a62cbc9959d
mwolfe13 <mtwolfe2@my.loyno.edu>
2026-05-22 19:16:25 -0500

    disable middleware

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,5 @@
-import { NextResponse, type NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
-export async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
-  const token = request.cookies.get('sb-jaohzcmrtfsrpeayekfy-auth-token')?.value
-
-  const isLoginPage = pathname.startsWith('/login')
-
-  if (!token && !isLoginPage) {
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
-
-  if (token && isLoginPage) {
-    return NextResponse.redirect(new URL('/dashboard', request.url))
-  }
-
+export function middleware() {
   return NextResponse.next()
-}
-
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
 }
